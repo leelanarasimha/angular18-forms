39289accac547246da401aaf89421e84d3a4dea4
LeelaNarasimha <LeelaNarasimha@teamvelocitymarketing.com>
2024-10-30 19:51:40 +0530

    dsdsd

--- src/app/reactive-form/reactive-form.component.spec.ts
+++ src/app/reactive-form/reactive-form.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ReactiveFormComponent } from './reactive-form.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 describe('ReactiveFormComponent', () => {
   let component: ReactiveFormComponent;
@@ -8,16 +9,28 @@ describe('ReactiveFormComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ReactiveFormComponent]
-    })
-    .compileComponents();
+      imports: [ReactiveFormComponent, ReactiveFormsModule],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(ReactiveFormComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should update the value of the input field', () => {
+    const input = fixture.nativeElement.querySelector('input');
+
+    const event = new Event('input');
+    input.value = 'Red';
+    input.dispatchEvent(event);
+
+    expect(component.favoriteColorControl.value).toEqual('Red');
+  });
+
+  it('should update the value in the control', () => {
+    component.favoriteColorControl.setValue('Blue');
+
+    const input = fixture.nativeElement.querySelector('input');
+    expect(input.value).toBe('Blue');
   });
 });
--- src/app/template-form/template-form.component.spec.ts
+++ src/app/template-form/template-form.component.spec.ts
@@ -1,6 +1,12 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  ComponentFixture,
+  fakeAsync,
+  TestBed,
+  tick,
+} from '@angular/core/testing';
 
 import { TemplateFormComponent } from './template-form.component';
+import { FormsModule } from '@angular/forms';
 
 describe('TemplateFormComponent', () => {
   let component: TemplateFormComponent;
@@ -8,16 +14,28 @@ describe('TemplateFormComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [TemplateFormComponent]
-    })
-    .compileComponents();
+      imports: [TemplateFormComponent, FormsModule],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(TemplateFormComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+  it('should update the favorite color in the component', fakeAsync(() => {
+    const input = fixture.nativeElement.querySelector('input');
+    input.value = 'Red';
+    input.dispatchEvent(new Event('input'));
+
+    fixture.detectChanges();
+    expect(component.favoriteColor).toEqual('Red');
+  }));
+
+  it('should update thefavorite color on the input field', fakeAsync(() => {
+    component.favoriteColor = 'Blue';
+    fixture.detectChanges();
+    tick();
+    const input = fixture.nativeElement.querySelector('input');
+    expect(input.value).toBe('Blue');
+  }));
 });
