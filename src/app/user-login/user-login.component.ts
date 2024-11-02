e0c8faa73630087da65290a7455b3ad73aa7cc0b
LeelaNarasimha <LeelaNarasimha@teamvelocitymarketing.com>
2024-11-02 19:01:05 +0530

    fdfdf

--- src/app/user-login/user-login.component.ts
+++ src/app/user-login/user-login.component.ts
@@ -7,12 +7,6 @@ import {
   UntypedFormGroup,
 } from '@angular/forms';
 
-interface User {
-  name: FormControl<string>;
-  age: FormControl<number>;
-  email: FormControl<string>;
-}
-
 @Component({
   selector: 'app-user-login',
   standalone: true,
@@ -21,31 +15,9 @@ interface User {
   styleUrl: './user-login.component.css',
 })
 export class UserLoginComponent {
-  fb = inject(FormBuilder);
+  emailControl = new FormControl('angularrox@gmail.com', { nonNullable: true });
 
-  userLoginForm = new UntypedFormGroup({
-    name: new UntypedFormControl(),
-    age: new UntypedFormControl(),
-    email: new UntypedFormControl(),
-  });
-
-  ngOnInit() {
-    this.userLoginForm.controls['age'].valueChanges.subscribe((data) => {
-      console.log(data);
-    });
-  }
-
-  onSubmit(event: Event) {
-    event.preventDefault();
-
-    const age = this.userLoginForm.get('age')?.value;
-  }
-
-  updateValues() {
-    this.userLoginForm.setValue({
-      name: 'dsdsd',
-      age: 23,
-      email: 'dsdsd',
-    });
+  resetEmail() {
+    this.emailControl.reset();
   }
 }
